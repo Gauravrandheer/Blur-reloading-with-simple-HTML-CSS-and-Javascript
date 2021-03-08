2edf4cb3c400e11e53e4ffb708c54644a5cb9735
gauravRandheer <gaurav.shrivastava.15csc@bml.edu.in>
2021-03-08 19:33:45 +0530

    myfirstreloading

---
+++ Blur-Reloding/main.js
@@ -0,0 +1,22 @@
+const bg = document.querySelector(`.bg`)
+const progress = document.querySelector(`.progress`)
+
+var load =0;
+
+let int = setInterval(loading,30);
+
+function loading(){
+  load++
+  if(load>99){
+    clearInterval(int);
+  }
+
+  progress.innerHTML = load +"%"
+  progress.style.opacity = scale(load,0,100,1,0)
+  bg.style.filter = `blur(${scale(load,0,100,30,0)}px)`
+  console.log(load);
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
